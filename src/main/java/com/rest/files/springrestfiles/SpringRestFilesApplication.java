6afa6be7abc0365ff7f59c27d8428c7b3e45fef4
Wadoz <vadikds@gmail.com>
2018-08-07 19:11:41 +0300

    init

---
+++ src/main/java/com/rest/files/springrestfiles/SpringRestFilesApplication.java
@@ -0,0 +1,12 @@
+package com.rest.files.springrestfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestFilesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rest/files/springrestfiles/SpringRestFilesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rest.files.springrestfiles;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRestFilesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
