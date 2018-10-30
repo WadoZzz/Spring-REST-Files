5c950ea0506a6f89c117d39fc69df769f8df98e1
Wadoz <vadikds@gmail.com>
2018-10-30 07:17:27 +0200

    rewrite code

--- src/main/java/com/rest/files/springrestfiles/controller/FileController.java
+++ src/main/java/com/rest/files/springrestfiles/controller/FileController.java
@@ -1,15 +1,5 @@
 package com.rest.files.springrestfiles.controller;
 
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.FileCopyUtils;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -22,6 +12,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping(value = "file")
@@ -36,7 +42,7 @@ public class FileController {
 	 * Upload file on server
 	 */
 	@PostMapping(value = "/upload")
-	public ResponseEntity upload(@RequestParam("files") MultipartFile[] files) {
+	public ResponseEntity<?> upload(@RequestParam("files") MultipartFile[] files) {
 		String uploadedFileName = Arrays.stream(files).map(MultipartFile::getOriginalFilename)
 				.filter(x -> !StringUtils.isEmpty(x)).collect(Collectors.joining(" , "));
 		if (StringUtils.isEmpty(uploadedFileName)) {
@@ -52,7 +58,7 @@ public class FileController {
 	 * Get file from server
 	 */
 	@GetMapping(value = "/{fileName}")
-	public ResponseEntity download(HttpServletResponse response, @PathVariable("fileName") String fileName) {
+	public ResponseEntity<?> download(HttpServletResponse response, @PathVariable("fileName") String fileName) {
 		try (InputStream inputStream = new BufferedInputStream(
 				new FileInputStream(uploadFolder + File.separator + fileName))) {
 			FileCopyUtils.copy(inputStream, response.getOutputStream());
@@ -71,7 +77,7 @@ public class FileController {
 	 * Rename file from server
 	 */
 	@PutMapping(value = "/{fileName}")
-	public ResponseEntity rename(@PathVariable("fileName") String file, @RequestParam String renameFile) {
+	public ResponseEntity<?> rename(@PathVariable("fileName") String file, @RequestParam String renameFile) {
 		File oldFile1 = new File(uploadFolder + File.separator + file);
 		File newFile2 = new File(uploadFolder + File.separator + renameFile);
 		if (oldFile1.renameTo(newFile2)) {
@@ -87,7 +93,7 @@ public class FileController {
 	 * Delete file from server
 	 */
 	@DeleteMapping(value = "/{fileName}")
-	public ResponseEntity delete(@PathVariable("fileName") String file) {
+	public ResponseEntity<?> delete(@PathVariable("fileName") String file) {
 		File deleteFile = new File(uploadFolder + File.separator + file);
 		if (deleteFile.exists()) {
 			deleteFile.delete();
